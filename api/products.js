4dc9609ca0ef3d331e191ebe987c2279629fb814
Artur B <predator777799@gmail.com>
2020-01-05 17:29:38 +0300

    additions to creating a server project

--- api/products.js
+++ api/products.js
@@ -1,22 +1,34 @@
 const express = require('express');
 
 const router = express.Router();
+const mongoose = require('mongoose');
 
+const produc = require('./produc');
 router.get('/', (req,res,next) => {
     res.status(200).json({
         message: 'Handling GET requests to /products'
     });
 });
-
-
 router.post('/', (req,res,next) => {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+       name:req.body.name,
+       price:req.body.price
+    });
+    product
+    .save()
+    .then(result => {
+        console.log(result);
+    })
+    .catch(err => console.log(err));
     res.status(201).json({
-        message: 'Handling POST requests to /products'
+        message: 'Handling POST requests to /products',
+        createdProduct: product
     });
 });
 
 router.get('/:productID', (req, res, next) => {
-    const id = req.params.productID;
+    const id = req.params.productId;
     if (id === 'special'){
         res.status(200).json({
             message: 'You descovered the special ID',
@@ -31,13 +43,13 @@ router.get('/:productID', (req, res, next) => {
 
 
 
-router.patch('/:productID', (req, res, next) => {
+router.patch('/:productId', (req, res, next) => {
     res.status(200).json({
         message: 'Updated products!'
     });
 });
 
-router.delete('/:productID', (req, res, next) => {
+router.delete('/:productId', (req, res, next) => {
     res.status(200).json({
         message: 'Deleted products!'
     });
