1d1b709a6375639b05e191e4601e16a4c609d7e1
Artur B <predator777799@gmail.com>
2020-01-06 15:37:05 +0300

    12

--- api/orders.js
+++ api/orders.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const Order = require('./ord');
+const Product = require('./produc');
 
 // Handle incoming GET requests to /orders
 router.get('/', (req,res,next) => {
    Order.find()
    .select('product quantity _id')
+       .populate('product')
    .exec()
    .then(docs => {
        res.status(200).json({
@@ -34,50 +36,84 @@ router.get('/', (req,res,next) => {
    });
 });
 router.post('/', (req,res,next) => {
-    const order = new Order ({
-        _id:mongoose.Types.ObjectId(),
-        quantity:req.body.quantity,
-        product:req.body.productId
-    });
-    order
-    .save()
-    .then(result => {
-        console.log(result);
-        res.status(201).json({
-            message:'Order stored',
-            createdOrder: {
-                _id:result._id,
-                product:result.product,
-                quantity:result.quantity
-            },
-            request: {
-                type: 'GET',
-                url: 'http://localhost:300/orders/' + result._id
-            }
+    Product.findById(req.body.productId)
+        .then(product => {
+            const order = new Order ({
+                _id:mongoose.Types.ObjectId(),
+                quantity:req.body.quantity,
+                product:req.body.productId
+            });
+            order
+                .save()
+                .then(result => {
+                    console.log(result);
+                    res.status(201).json({
+                        message:'Order stored',
+                        createdOrder: {
+                            _id:result. _id,
+                            product:result.product,
+                            quantity:result.quantity
+                        },
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:300/orders/' + result. _id
+                        }
+                    });
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json({
+                        error:err
+                    });
+                });
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Product not found',
+                error:err
+            });
         });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            error:err
-        });
-    });
 });
-
 router.get('/:orderID', (req,res,next) => {
-    res.status(200).json({
-        message: 'Order details',
-        orderID: req.params.orderID
-    });
+    Order.findById(req.params.orderId)
+        .exec()
+        .then(order => {
+            if (!order) {
+                return res.status(404).json({
+                    message: "Order not found"
+                });
+            }
+            res.status(200).json({
+                order: order,
+                request: {
+                    type: "GET",
+                    url: "http://localhost:3000/orders"
+                }
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
 });
-
 router.delete('/:orderID', (req,res,next) => {
-    res.status(200).json({
-        message: 'Order deleted',
-        orderID: req.params.orderID
-    });
+    Order.remove({ _id: req.params.orderId })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: "Order deleted",
+                request: {
+                    type: "POST",
+                    url: "http://localhost:3000/orders",
+                    body: { productId: "ID", quantity: "Number" }
+                }
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
 });
-
-
 module.exports = router;
-
