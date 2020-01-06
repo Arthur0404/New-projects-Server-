c02d9e5b35efeb772f672c5e0ddbfbb3a96f56c4
Artur B <predator777799@gmail.com>
2020-01-06 13:11:50 +0300

    Mongo DB Atlas

--- api/orders.js
+++ api/orders.js
@@ -1,21 +1,66 @@
 const express = require('express');
 
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Order = require('./ord');
 
 // Handle incoming GET requests to /orders
 router.get('/', (req,res,next) => {
-    res.status(200).json({
-        message: 'Orders were fetched'
-    });
+   Order.find()
+   .select('product quantity _id')
+   .exec()
+   .then(docs => {
+       res.status(200).json({
+           count:docs.length,
+           order:docs.map(doc => {
+               return {
+                   _id:doc._id,
+                   product:doc.product,
+                   quantity:doc.quantity,
+                   request: {
+                       type: 'GET',
+                       url: 'http://localhost:300/orders/' + doc._id
+                   }
+               }
+           })
+           
+       });
+   })
+   .catch(err => {
+       res.status(500).json({
+           error:err
+       });
+   });
 });
 router.post('/', (req,res,next) => {
-    const order = {
-        productId: req.body.productId,
-        quantity: req.body.quantity
-    };
-    res.status(201).json({
-        message: 'Order was created',
-        order:order
+    const order = new Order ({
+        _id:mongoose.Types.ObjectId(),
+        quantity:req.body.quantity,
+        product:req.body.productId
+    });
+    order
+    .save()
+    .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message:'Order stored',
+            createdOrder: {
+                _id:result._id,
+                product:result.product,
+                quantity:result.quantity
+            },
+            request: {
+                type: 'GET',
+                url: 'http://localhost:300/orders/' + result._id
+            }
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error:err
+        });
     });
 });
 
@@ -35,3 +80,4 @@ router.delete('/:orderID', (req,res,next) => {
 
 
 module.exports = router;
+
